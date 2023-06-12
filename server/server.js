ee3de129f050bfc5e4a34c865f39e8e617f1d22b
Alejandro Agra <aleagra08@hotmail.com>
2023-06-12 16:47:28 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const User = require("./models/users.models");
 const app = express();
 const socketIO = require("socket.io");
 const multer = require("multer");
-const multerGoogleStorage = require('multer-google-storage');
 const admin = require('firebase-admin');
 const path = require("path");
 
@@ -25,11 +24,16 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+const serviceAccountKeyPath = path.resolve(__dirname, "./storage/storage.json");
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  storageBucket: 'social-app-751c3.appspot.com'
+  credential: admin.credential.cert(serviceAccountKeyPath),
+  storageBucket: 'socialapp-storage-94b01.appspot.com'
 });
-const storage = admin.storage();
+
+const bucket = admin.storage().bucket();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 app.use("/users", authRoutes);
 app.use("/messages", messageRoutes);
@@ -76,64 +80,78 @@ io.on("connection", (socket) => {
 
 
   socket.on("follow-user", async (data) => {
-  const user = await User.findById(data.userId);
-  const follower = await User.findById(data.followerId);
+    const user = await User.findById(data.userId);
+    const follower = await User.findById(data.followerId);
 
-  if (user && follower) {
-    if (!user.following.includes(data.followerId)) {
-      user.following.push(data.followerId);
-      follower.followers.push(data.userId);
+    if (user && follower) {
+      if (!user.following.includes(data.followerId)) {
+        user.following.push(data.followerId);
+        follower.followers.push(data.userId);
 
-      await user.save();
-      await follower.save();
+        await user.save();
+        await follower.save();
 
-      io.emit("follower-count-updated", {
-        userId: data.userId,
-        followerCount: user.following.length,
-      });
+        io.emit("follower-count-updated", {
+          userId: data.userId,
+          followerCount: user.following.length,
+        });
+      }
+    } else {
+      console.log("No se encontraron los documentos de usuario");
     }
-  } else {
-    console.log("No se encontraron los documentos de usuario");
+  });
+
+  socket.on("unfollow-user", async (data) => {
+    const user = await User.findById(data.userId);
+    const follower = await User.findById(data.followerId);
+
+    if (user && follower) {
+      if (user.following.includes(data.followerId)) {
+        user.following.pull(data.followerId);
+        follower.followers.pull(data.userId);
+
+        await user.save();
+        await follower.save();
+
+        io.emit("follower-count-updated", {
+          userId: data.userId,
+          followerCount: user.following.length,
+        });
+      }
+    } else {
+      console.log("No se encontraron los documentos de usuario");
+    }
+  });
+});
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    const file = req.file;
+    if (!file) {
+      return res.status(400).json('No se ha seleccionado ningún archivo');
+    }
+    
+    const fileName = new Date().getTime() + file.originalname;
+    const fileRef = bucket.file(fileName);
+    const fileStream = fileRef.createWriteStream({
+      metadata: {
+        contentType: file.mimetype,
+      },
+    });
+
+    fileStream.on("error", (error) => {
+      console.error(error);
+      res.status(500).json('Error al subir el archivo');
+    });
+
+    fileStream.on("finish", () => {
+      const downloadURL = `https://storage.googleapis.com/${bucket.name}/${fileName}`;
+      res.status(200).json(downloadURL);
+    });
+
+    fileStream.end(file.buffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json('Error al subir el archivo');
   }
 });
-socket.on("unfollow-user", async (data) => {
-  const user = await User.findById(data.userId);
-  const follower = await User.findById(data.followerId);
-
-  if (user && follower) {
-    if (user.following.includes(data.followerId)) {
-      user.following.pull(data.followerId);
-      follower.followers.pull(data.userId);
-
-      await user.save();
-      await follower.save();
-
-      io.emit("follower-count-updated", {
-        userId: data.userId,
-        followerCount: user.following.length,
-      });
-    }
-  } else {
-    console.log("No se encontraron los documentos de usuario");
-  }
-});
-
-});
-
-// const upload = multer({
-//   storage: multerGoogleStorage.storageEngine({
-//     bucket: 'social-app-751c3.appspot.com',
-//     projectId: 'social-app-751c3',
-//     filename: (req, file, cb) => {
-//       cb(null, file.originalname);
-//     }
-//   })
-// });
-
-// app.post('/upload', upload.single('file'), (req, res) => {
-//   try {
-//     return res.status(200).json('Archivo subido correctamente');
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
