d0fa804c096815f411597362f790b23c001e402b
Alejandro Agra <aleagra08@hotmail.com>
2023-06-13 18:41:57 -0300

    fix

--- server/server.js
+++ server/server.js
@@ -2,54 +2,41 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const multer = require("multer");
+const admin = require("firebase-admin");
+const path = require("path");
+const socketIO = require("socket.io");
+const bodyParser = require("body-parser");
+
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 const postRoutes = require("./routes/posts.route");
 const User = require("./models/users.models");
+
 const app = express();
-const socketIO = require("socket.io");
-const multer = require("multer");
-const admin = require('firebase-admin');
-const path = require("path");
-
-app.use(express.json());
-app.use(express.static(__dirname));
-
-app.use(cors({
-  origin: 'https://social-application.web.app',
-}));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server started on ${process.env.PORT}`);
 });
 
-// Initialize Firebase Admin
+const io = socketIO(server, {
+  cors: {
+    origin: "https://social-application.web.app",
+    credentials: true,
+  },
+});
+
 const serviceAccountKeyPath = path.join(__dirname, "storage/storage.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKeyPath),
-  storageBucket: 'socialapp-storage-94b01.appspot.com'
+  storageBucket: "socialapp-storage-94b01.appspot.com",
 });
 
 const bucket = admin.storage().bucket();
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-// Routes
-app.use("/users", authRoutes);
-app.use("/messages", messageRoutes);
-app.use(postRoutes);
-app.use("/images", express.static(path.join(__dirname, "public/images")));
-
-// Database connection
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true, // Add this line for index creation
-  })
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => {
     console.log("DB Connection Successful");
   })
@@ -57,19 +44,29 @@ mongoose
     console.log(err.message);
   });
 
-// Start the server
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
-);
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors({
+  origin: "https://social-application.web.app",
+}));
 
-// Socket.io configuration
-const io = socketIO(server, {
-  cors: {
-    origin: "https://social-application.web.app",
-    credentials: true,
-  },
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://social-application.web.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
 });
-mongoose.set("strictPopulate", false);
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+app.use("/users", authRoutes);
+app.use("/messages", messageRoutes);
+app.use(postRoutes);
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+
 global.onlineUsers = new Map();
 app.set("io", io);
 
@@ -87,56 +84,48 @@ io.on("connection", (socket) => {
   });
 
   socket.on("follow-user", async (data) => {
-    try {
-      const [user, follower] = await Promise.all([
-        User.findById(data.userId),
-        User.findById(data.followerId),
-      ]);
+    const user = await User.findByIdAndUpdate(
+      data.userId,
+      { $addToSet: { following: data.followerId } },
+      { new: true }
+    );
 
-      if (user && follower) {
-        if (!user.following.includes(data.followerId)) {
-          user.following.push(data.followerId);
-          follower.followers.push(data.userId);
+    const follower = await User.findByIdAndUpdate(
+      data.followerId,
+      { $addToSet: { followers: data.userId } },
+      { new: true }
+    );
 
-          await Promise.all([user.save(), follower.save()]);
-
-          io.emit("follower-count-updated", {
-            userId: data.userId,
-            followerCount: user.following.length,
-          });
-        }
-      } else {
-        console.log("No se encontraron los documentos de usuario");
-      }
-    } catch (error) {
-      console.log(error.message);
+    if (user && follower) {
+      io.emit("follower-count-updated", {
+        userId: data.userId,
+        followerCount: user.following.length,
+      });
+    } else {
+      console.log("No se encontraron los documentos de usuario");
     }
   });
 
   socket.on("unfollow-user", async (data) => {
-    try {
-      const [user, follower] = await Promise.all([
-        User.findById(data.userId),
-        User.findById(data.followerId),
-      ]);
+    const user = await User.findByIdAndUpdate(
+      data.userId,
+      { $pull: { following: data.followerId } },
+      { new: true }
+    );
 
-      if (user && follower) {
-        if (user.following.includes(data.followerId)) {
-          user.following.pull(data.followerId);
-          follower.followers.pull(data.userId);
+    const follower = await User.findByIdAndUpdate(
+      data.followerId,
+      { $pull: { followers: data.userId } },
+      { new: true }
+    );
 
-          await Promise.all([user.save(), follower.save()]);
-
-          io.emit("follower-count-updated", {
-            userId: data.userId,
-            followerCount: user.following.length,
-          });
-        }
-      } else {
-        console.log("No se encontraron los documentos de usuario");
-      }
-    } catch (error) {
-      console.log(error.message);
+    if (user && follower) {
+      io.emit("follower-count-updated", {
+        userId: data.userId,
+        followerCount: user.following.length,
+      });
+    } else {
+      console.log("No se encontraron los documentos de usuario");
     }
   });
 });
