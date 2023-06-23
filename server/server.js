024ab6b01cd2583ca4cd3fac6a9b8bb726c131f9
Alejandro Agra <aleagra08@hotmail.com>
2023-06-23 16:08:20 -0300

    delete storage

--- server/server.js
+++ server/server.js
@@ -25,12 +25,24 @@ app.use((req, res, next) => {
   next();
 });
 
-const serviceAccountKeyPath = path.join(__dirname, "storage/storage.json");
+const serviceAccount = {
+  type: process.env.SERVICE_ACCOUNT_TYPE,
+  project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+  private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+  private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+  client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
+  auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
+  token_uri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.SERVICE_ACCOUNT_UNIVERSE_DOMAIN,
+};
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountKeyPath),
+  credential: admin.credential.cert(serviceAccount),
   storageBucket: 'socialapp-storage-94b01.appspot.com'
 });
-
 const bucket = admin.storage().bucket();
 
 const storage = multer.memoryStorage();
